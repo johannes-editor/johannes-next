b97996118826dc06a0ee91e469dc31b2e8292863
Alexandre <alexandretavares@outlook.com>
2025-06-20 14:55:44 -0300

    Update nested component example

--- src/editor.tsx
+++ src/editor.tsx
@@ -1,14 +1,16 @@
 /** @jsx h */
 import { h } from './jsx.ts';
-import { t, setLocale } from './i18n/mod.ts';
+import { t, setLocale } from "./i18n/index.ts";
+import { CounterToggle } from './components/CounterToggle.tsx';
 
 export { setLocale };
 
-export function initEditor(root: HTMLElement) {
+export async function initEditor(root: HTMLElement) {
   const lang = root.getAttribute('lang') || 'en';
-  setLocale(lang);
+  await setLocale(lang);
   root.replaceChildren(
     <h1 data-placeholder={t('untitled')}></h1>,
     <p data-placeholder={t('startTyping')} ></p>,
+    <CounterToggle />,
   );
 }
