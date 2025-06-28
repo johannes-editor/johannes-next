d4fdd1f72eaf8f0b2094ec4df35ebf8a7d02b8be
Alexandre <alexandretavares@outlook.com>
2025-06-28 18:46:42 -0300

    Moved and updated Counter and CounterToggle as nested component examples in sample

---
+++ src/components/samples/counter-toggle.tsx
@@ -0,0 +1,53 @@
+/** @jsx h */
+import { Fragment, h } from "../../jsx.ts";
+import { Component } from "../../component.ts";
+import { Counter } from "./counter.tsx";
+import { EmptyProps } from "../types.ts";
+
+interface CounterToggleState {
+    mounted: boolean;
+    count: number;
+}
+
+export class CounterToggle extends Component<EmptyProps, CounterToggleState> {
+
+    styles = /*css*/ `
+        .counter-holder {
+            padding: 10px;
+            background: #eee;
+        }
+        button {
+            color: green;
+        }
+    `;
+
+    constructor() {
+        super();
+
+        this.state = {
+            mounted: true,
+            count: 1,
+        };
+    }
+
+    private increment = () => {
+        console.log("incremented");
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    private toggle = () => {
+        this.setState({ mounted: !this.state.mounted });
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <div class="counter-holder">
+                    <span>mounted: {String(this.state.mounted)}</span>
+                    {this.state.mounted && <Counter count={this.state.count} increment={this.increment} />}
+                </div>
+                <button type="button" onClick={this.toggle}>Toggle Counter</button>
+            </Fragment>
+        );
+    }
+}
---
+++ src/components/samples/counter.tsx
@@ -0,0 +1,43 @@
+/** @jsx h */
+import { h } from "../../jsx.ts";
+import { Component } from "../../component.ts";
+
+export interface CounterProps {
+    count: number;
+    increment: () => void;
+}
+
+export class Counter extends Component<CounterProps> {
+    override state = { count: 1 };
+
+    static styles = `ba`;
+
+    private readonly handleKey = (e: KeyboardEvent) => {
+        if (e.key === "Escape") {
+            this.state.count = this.props.count ?? 0;
+        }
+
+        if (e.key === "a") {
+            console.log("a pressed");
+        }
+    };
+
+    override onMount() {
+        console.log("mounted element");
+
+        this.on(document, "keydown", this.handleKey as EventListener);
+    }
+
+    override onUnmount() {
+        console.log("unmounted element");
+    }
+
+    render() {
+        return (
+            <div>
+                <span>{this.props.count}</span>
+                <button onClick={this.props.increment}>+</button>
+            </div>
+        );
+    }
+}
