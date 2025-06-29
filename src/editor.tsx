e83506a0f610056801a99551daae9cc1fdb5c785
Alexandre <alexandretavares@outlook.com>
2025-06-28 22:13:27 -0300

    Updated the editor

--- src/editor.tsx
+++ src/editor.tsx
@@ -1,16 +1,30 @@
 /** @jsx h */
-import { h } from './jsx.ts';
-import { t, setLocale } from "./i18n/index.ts";
-import { CounterToggle } from './components/CounterToggle.tsx';
+import { Fragment, h } from "./jsx.ts";
+import { setLocale, t } from "./i18n/index.ts";
+import { CounterToggle } from "./components/samples/counter-toggle.tsx";
+import { SlashMenu } from "./components/samples/slash-menu/slash-menu.tsx";
+import { loadActivePlugins } from "./plugins/loader.ts";
+import { SimpleButton } from "./components/samples/simple-button.tsx";
+import { FetchButton } from "./components/samples/fetch-component/fetch-component.tsx";
 
 export { setLocale };
 
 export async function initEditor(root: HTMLElement) {
-  const lang = root.getAttribute('lang') || 'en';
-  await setLocale(lang);
-  root.replaceChildren(
-    <h1 data-placeholder={t('untitled')}></h1>,
-    <p data-placeholder={t('startTyping')} ></p>,
-    <CounterToggle />,
-  );
+    const lang = root.getAttribute("lang") || "en";
+    await setLocale(lang);
+
+    const plugins = await loadActivePlugins();
+
+    root.replaceChildren(
+        <Fragment>
+            <div id="content" contenteditable="true">
+                <h1 data-placeholder={t("untitled")}></h1>
+                <p data-placeholder={t("startTyping")}></p>
+            </div>
+            <SimpleButton />
+            <CounterToggle />
+            <SlashMenu plugins={plugins} />
+            <FetchButton />
+        </Fragment>
+    );
 }
