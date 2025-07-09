02411070f8eab9ebc45a8d43ec92527828c7a245
Alexandre <alexandretavares@outlook.com>
2025-07-09 15:10:18 -0300

    Add utility classes for event types and keyboard keys

---
+++ src/utils/event-types.ts
@@ -0,0 +1,22 @@
+export class EventTypes {
+    static readonly KeyDown = 'keydown';
+    static readonly KeyUp = 'keyup';
+    static readonly KeyPress = 'keypress';
+    static readonly MouseDown = 'mousedown';
+    static readonly MouseUp = 'mouseup';
+    static readonly MouseMove = 'mousemove';
+    static readonly Click = 'click';
+    static readonly DblClick = 'dblclick';
+    static readonly ContextMenu = 'contextmenu';
+    static readonly Focus = 'focus';
+    static readonly Blur = 'blur';
+    static readonly Change = 'change';
+    static readonly Input = 'input';
+    static readonly Submit = 'submit';
+    static readonly Scroll = 'scroll';
+    static readonly Resize = 'resize';
+    static readonly Drag = 'drag';
+    static readonly Drop = 'drop';
+    static readonly DragStart = 'dragstart';
+    static readonly DragEnd = 'dragend';
+}
