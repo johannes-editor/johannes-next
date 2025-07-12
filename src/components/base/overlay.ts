0da49eb1f547d67ed49cae6067dc66f7ebbc968d
Alexandre <alexandretavares@outlook.com>
2025-07-12 20:25:44 -0300

    Add Overlay class for building floating UI components

---
+++ src/components/base/overlay.ts
@@ -0,0 +1,20 @@
+import { OverlayManager } from "../../core/overlay-manager.ts";
+import { Component } from "../component.ts";
+
+// deno-lint-ignore no-explicit-any
+export abstract class Overlay<P = any, S = any> extends Component<P, S> {
+
+    zIndex: number = 1000;
+
+    override connectedCallback(): void {
+        super.connectedCallback();
+
+        this.style.zIndex = this.zIndex.toString();
+        OverlayManager.instance().push(this);
+    }
+
+    override disconnectedCallback(): void {
+        super.disconnectedCallback();
+        OverlayManager.instance().remove(this);
+    }
+}
